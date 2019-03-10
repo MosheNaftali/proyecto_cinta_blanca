fbf1360f236c05c2e6bae06e67ce2e82b9f151eb
Moshe Shabes <moshnaf@gmail.com>
2019-03-10 01:02:21 -0600

    Se vuelve agregar archivos por error

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var i=0;
+function alertaCarro(){
+    alert("Se ha agregado el producto al carro")
+    i += 1;
+    carro.innerHTML = i;
+}
+function alertaComprar(){
+    alert("Acción no disponible")
+}
